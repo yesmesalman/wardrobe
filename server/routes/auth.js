1e0fe108a9357d514d77836f4f618771177c6977
Salman Saleem <salmanmemon569@yahoo.com>
2023-02-07 14:16:23 +0500

    server routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const helpers = require('./../helpers/helpers');
 
-router.get('/', (req, res) => {
+router.post('/', (req, res) => {
   return helpers.successResponse(res, "Welcome", []);
 });
 
-router.get('/register-device', (req, res) => {
+router.post('/register-device', (req, res) => {
   return helpers.successResponse(res, "register device", req.body);
 });
 
