14ec78065523f41d5e7391e159c47c64602962b0
Salman Saleem <37062860+yesmesalman@users.noreply.github.com>
2024-02-15 23:40:15 +0500

    clean

--- server/routes/auth/index.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-const router = Router();
-import { registerDeviceValidation, registerDevice } from './devices.js';
-
-router.post('/register-device', registerDeviceValidation, registerDevice);
-
-export default router;
